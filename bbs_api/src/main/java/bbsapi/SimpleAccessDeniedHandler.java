99edc0f51e754e4003b6611fcd93179fc05dc632
Godyso <113145633+Godyso@users.noreply.github.com>
2022-11-29 17:24:25 -0500

    Add files via upload

---
+++ bbs_api/src/main/java/bbsapi/SimpleAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package bbsapi;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+public class SimpleAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException exception) throws IOException, ServletException {
+        response.sendError(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN.getReasonPhrase());
+    }
+
+}
---
+++ bbs_api/src/main/java/bbsapi/app/book/BooksController.java
@@ -0,0 +1,37 @@
+package bbsapi.app.book;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import bbsapi.domain.model.Rating;
+import bbsapi.domain.service.book.RatingService;
+
+@RestController
+@RequestMapping("api/books")
+public class BooksController {
+	@Autowired
+	RatingService ratingService;
+
+	//Get all books
+	@RequestMapping(value="/all", method=RequestMethod.GET, produces = "application/json")
+	List<Rating> getAllBooksList() {
+		List<Rating> book = ratingService.findAll();
+		return book;
+	}
+
+	//Get book by genru
+	@RequestMapping(value="/genre/{genruId}", method=RequestMethod.GET, produces = "application/json")
+	List<Rating> getBooksByGenruId(@PathVariable Long genruId) {
+		List<Rating> book = ratingService.findByGenruId(genruId);
+		return book;
+	}
+
+	//Get book by bookId
+	@RequestMapping(method=RequestMethod.GET, produces = "application/json")
+	Rating getOneBookById(@RequestParam Long book_id) {
+		Rating book = ratingService.findOneByBookId(book_id);
+		return book;
+	}
+
+}
